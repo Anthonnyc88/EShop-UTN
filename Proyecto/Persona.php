f7bf10a84a66fdbd545d0a992babfe89aeda3a36
punts hola mundo <anthonnyc10@gmail.com>
2018-10-18 18:08:53 -0600

    Configuracion de la clase persona

--- Proyecto/Persona.php
+++ Proyecto/Persona.php
@@ -4,21 +4,22 @@ include "Conexion.php";
 
 //Se crea Clase Cliente con sus respectivos atributos publicos
 
-class Cliente{
+class Personas{
     public  $username;
-    public $contraseña;
+    public  $contraseña;
     public  $nombre;
     public  $apellido_1;
     public  $apellido_2;
     public  $numero_Telefono;
     public  $correo_Electronico;
     public  $direccion;
+    public $tipo_persona;
     
     
 
 
     //Metodo Constructor de la clase cliente
-    function __construct($username,$contraseña,$nombre , $apellido_1, $apellido_2, $numero_Telefono,$correo_Electronico , $direccion ){
+    function __construct($username,$contraseña,$nombre , $apellido_1, $apellido_2, $numero_Telefono,$correo_Electronico , $direccion,$tipo_persona ){
         $this->username=$username;
         $this->contraseña=$contraseña;
         $this->nombre=$nombre;
@@ -27,17 +28,16 @@ class Cliente{
         $this->numero_Telefono= $numero_Telefono;
         $this->correo_Electronico= $correo_Electronico;
         $this->direccion= $direccion;
+        $this->tipo_Persona=$tipo_persona;
         
-        
-
-
+    
      }
 
 //Función que registra un cliente al sistema y se guardara en una base de datos
-    function registrar_Cliente(){
+    function registrar_Persona(){
         try{
             $Conexion = Conexion();
-            $sql = "INSERT INTO clientes VALUES('$this->username','$this->contraseña,$this->nombre', '$this->apellido_1', '$this->apellido_2',$this->numero_Telefono, '$this->correo_Electronico','$this->direccion' )";
+            $sql = "INSERT INTO clientes VALUES('$this->username','$this->contraseña,$this->nombre', '$this->apellido_1', '$this->apellido_2',$this->numero_Telefono, '$this->correo_Electronico','$this->direccion' ,'$this->tipo_Persona')";
             $Conexion->query($sql);
             echo "Nuevo Cliente Agregado!";
         
