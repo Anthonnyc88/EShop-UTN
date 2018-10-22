ce9132c95a951f538616bab8a86677d486b732f5
GabrielQuesadaAraya <gabrielquesadajgqa@gmail.com>
2018-10-21 23:00:20 -0600

    Registro del cliente

--- Proyecto/php/Persona.php
+++ Proyecto/php/Persona.php
@@ -1,73 +1,80 @@
 <?php
 //Se importa la clase conexion hacia la clase cliente para asi trabajar en manera simultania
 include "Conexion.php";
-
 //Se crea Clase Cliente con sus respectivos atributos publicos
-
 class Personas{
     public  $username;
-    public  $contraseña;
+    public  $contrasena;
     public  $nombre;
     public  $id;
     public  $apellido_1;
     public  $apellido_2;
-    public  $numero_Telefono;
-    public  $correo_Electronico;
+    public  $telefono;
+    public  $correo;
     public  $direccion;
     
     
 
 
     //Metodo Constructor de la clase cliente
-    function __construct($username,$contraseña,$nombre ,$id, $apellido_1, $apellido_2, $numero_Telefono,$correo_Electronico , $direccion ){
+    function __construct($username,$contrasena,$nombre ,$id, $apellido_1, $apellido_2, $telefono,$correo , $direccion){
         $this->username=$username;
-        $this->contraseña=$contraseña;
+        $this->contrasena=$contrasena;
         $this->nombre=$nombre;
         $this->id=$id;
         $this->apellido_1= $apellido_1;
         $this->apellido_2= $apellido_2;
-        $this->numero_Telefono= $numero_Telefono;
-        $this->correo_Electronico= $correo_Electronico;
+        $this->telefono= $telefono;
+        $this->correo= $correo;
         $this->direccion= $direccion;
-       
         
     
-     }
     }
-     
-     function metodo_Post(){
-        if(isset($_POST)){
-            $this->username = $_POST["username"];
-            $this->contraseña = $_POST["contraseña"];
-            $this->nombre = $_POST["nombre"];
-            $this->id = $_POST["id"];
-            $this->apellido_1 = $_POST["apellido_1"];
-            $this->apellido_2 = $_POST["apellido_2"];
-            $this->telefono = $_POST["telefono"];
-            $this->correo = $_POST["correo"];
-            $this->direccion = $_POST["direccion"];
+     //Función que registra una Persona al sistema y se guardara en una base de datos
+     function registrar_Persona(){
+            $conexion = Conexion();
+            $sql = "INSERT INTO personas(username,contrasena,nombre,id,apellido_1,apellido_2,telefono,correo,direccion) VALUES('$this->username','$this->contrasena','$this->nombre','$this->id','$this->apellido_1','$this->apellido_2','$this->telefono','$this->correo','$this->direccion')";
+            $conexion->query($sql);
+            }    
+}
+//Funcion que resive los datos ingresados por el cliente a la hora de registrarse
+function post(){
+include "Registrar.html";
+ if(isset($_POST)){
+    $username = $_POST["username"];
+    $contrasena = $_POST["contrasena"];
+    $nombre = $_POST["nombre"];
+    $id = $_POST["id"];
+    $apellido_1 = $_POST["apellido_1"];
+    $apellido_2 = $_POST["apellido_2"];
+    $telefono = $_POST["telefono"];
+    $correo = $_POST["correo"];
+    $direccion = $_POST["direccion"];
+    $clase = new Personas($this->username,$this->contrasena,$this->nombre,$this->id,$this->apellido_1,$this->apellido_2,$this->telefono,$this->correo,$this->direccion);
+    $clase->registrar_Persona();
+}
+}    
 
-     }
+
         
-        }
-
-//Función que registra una Persona al sistema y se guardara en una base de datos
-
-        function registrar_Persona(){
+         
+//Función que verifica si los datos que inserto son los mismos que estan dentro de la base de datos
+        /* function Verificar_Persona(){
             try{
                 metodo_Post();
                 $Conexion = Conexion();
-                $sql = "INSERT INTO persona VALUES('$this->username','$this->contraseña','$this->nombre','$this->id', '$this->apellido_1', '$this->apellido_2','$this->numero_Telefono', '$this->correo_Electronico','$this->direccion' )";
+                $sql =("SELECT * FROM persona WHERE username = '$username' and contraseña='$contraseña'");
                 $Conexion->query($sql);
-                echo "Nuevo Cliente Agregado!";
+                if( Conexion($sql) == 0){
+                    // mysql_num_rows <- esta funcion me imprime el numero de registro que encontro 
+                    // si el numero es igual a 0 es porque el registro no exite, en otras palabras ese user no esta en la tabla miembro por lo tanto se puede registrar
+                     
+                    echo "el user es valido";
+                    }
+                echo "Datos correctos";
             
             }catch(Exception $error){
                 echo $error;
             }
-         }
-
-
-
-
-
+         }*/
 ?>
--- Proyecto/php/Registrar.php
+++ Proyecto/php/Registrar.php
@@ -1,68 +1,30 @@
-<?php
-
-    if(isset($_POST)){
-        $username = $_POST["username"];
-        $contraseña = $_POST["contraseña"];
-        $nombre = $_POST["nombre"];
-        $id = $_POST["id"];
-        $apellido_1 = $_POST["apellido_1"];
-        $apellido_2 = $_POST["apellido_2"];
-        $telefono = $_POST["telefono"];
-        $correo = $_POST["correo"];
-        $direccion = $_POST["direccion"];
-        $tipo_persona = $_POST["tipo_persona"];
-
-
-
-        try{
-            $conexion = new PDO("mysql:host=localhost;dbname=proyecto","root","");
-        }catch(PDOException $e){
-            echo "Error: " . $e->getMessage();
-            die();  
-        }
-
-        $sql = "INSERT INTO persona VALUES('$username', '$contraseña', '$nombre', '$id', '$apellido_1', '$apellido_2', '$telefono', '$correo', '$direccion', '$tipo_persona')";
-        $conexion->query($sql);
-
-        
-    }
-
+<?php 
+include "Persona.php";
+ if(isset($_POST)){
+    $username = $_POST["username"];
+    $contrasena = $_POST["contrasena"];
+    $nombre = $_POST["nombre"];
+    $id = $_POST["id"];
+    $apellido_1 = $_POST["apellido_1"];
+    $apellido_2 = $_POST["apellido_2"];
+    $telefono = $_POST["telefono"];
+    $correo = $_POST["correo"];
+    $direccion = $_POST["direccion"];
+    $clase = new Personas($username,$contrasena,$nombre,$id,$apellido_1,$apellido_2,$telefono,$correo,$direccion);
+    $clase->registrar_Persona();
+}
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    
-    <title>Registro</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-	 crossorigin="anonymous">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
-	 crossorigin="anonymous">
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Registro exitoso</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="../css/sty"></script>
 </head>
 <body>
-    
-<body>
-	<div class="container">
-		<div class="content w3-animate-zoom">
-					</div>
-					<form action="php/Registrar.php" method="post">
-						<div class="form-group">
-						</div>
-						<div class="row align-items-center remember">
-						</div>
-						<div class="content w3-animate-zoom" style="background: rgba(1, 2, 3, 0.4)">
-							<div class="d-flex justify-content-center links">
-                                 <h1>Registro exitoso</h1>
-							</div>
-					</form>
-					<div class="d-flex justify-content-center">
-                    <a href="javascript:history.back(-2);">Iniciar Sesion</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</body>
+    <h1>Registro exitoso Sarapiqui</h1>
 </body>
 </html>
